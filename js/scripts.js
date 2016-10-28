2eccbfdb89fbd6a51083c47b46d22f1464c30160
Justin Xu <justinjxu@yahoo.com>
2016-10-28 15:39:21 -0700

    add function to let user retype number without refresh the page

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,10 @@
 function count(num) {
   newNums = [];
   for (i=1; i<=num; i++) {
-
  	if ((i % 3===0 )&& (i % 15 !== 0) && (i % 5 !== 0))
     newNums.push("ping");
   else if ((i % 5===0 )&& (i % 15 !== 0) && (i % 3 !== 0))
     newNums.push("pong");
-
   else if ((i % 15 === 0) )
     	newNums.push("ping pong");
   else
@@ -18,20 +16,14 @@ function count(num) {
 //user interface
 $(document).ready(function() {
   $("form#input").submit(function(event) {
+    jQuery(this).unbind('submit'); // unbind this submit handler first and ...
     num = ($("input#number").val());
     results = count(num);
     results.forEach(function(result){
       $("ul.results").append('<li>' + "your output is "+ result + "!"+'</li>');
       $("#show").show();
     });
-
-    //$("#results").text(result);
-
-    //$.each(newNums, function(value){
-      //$("#results").text(value);
-      //$("#results").text("." + ":" + );
       event.preventDefault();
-    //$("#results").text(results);
   });
 
 });
